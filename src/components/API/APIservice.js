cf4f2cdcd48af2fe274346cb02f551203f13a155
Alexey Zyukov <aalex64@ukr.net>
2021-12-13 22:25:26 +0200

    update API

--- src/components/API/APIservice.js
+++ src/components/API/APIservice.js
@@ -4,14 +4,13 @@ const API_KEY = '22969480-c3583c2b4b1ca4646f49ed52f';
 const FetchImages = (nextName, page) => {
   return fetch(
     `${BASE_URL}?q=${nextName}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
-  )
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      return Promise.reject(new Error('Нет картинки с таким названием '));
-    })
-    .catch(error => this.setState({ error }));
+  ).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject(new Error('Нет картинки с таким названием '));
+  });
+  // .catch(error => this.setState({ error }));
 };
 
 export default FetchImages;
