bd42d6c37b4b09efbecb0aa2441a9597340528f0
Alexey Zyukov <aalex64@ukr.net>
2021-12-13 23:00:13 +0200

    preliminary GalleryItem

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import styles from '../styles.module.css';
 
-<li class={styles.galleryItem}>
-  <img src="" alt="" />
-</li>;
+export default function ImageGalleryItem({ id, tags, smallImage, largeImage }) {
+  return (
+    <li key={id} className={styles.galleryItem}>
+      <img
+        className={styles.galleryImg}
+        alt={tags}
+        src={smallImage}
+        data-source={largeImage}
+      />
+    </li>
+  );
+}
