c0b0b4e9eb775e5de4def3e93a1b368d7652252f
Alexey Zyukov <aalex64@ukr.net>
2021-12-13 20:01:00 +0200

    update

--- src/components/API/APIservice.js
+++ src/components/API/APIservice.js
@@ -1,6 +1,6 @@
 const BASE_URL = 'https://pixabay.com/api/';
 
-function fetchImages(pictureName, page) {
+function FetchImages(pictureName, page) {
   return fetch(
     `${BASE_URL}?q=${pictureName}&page=${page}&key=22969480-c3583c2b4b1ca4646f49ed52f&image_type=photo&orientation=horizontal&per_page=12`,
   ).then(response => {
@@ -10,5 +10,5 @@ function fetchImages(pictureName, page) {
     return response.json();
   });
 }
-const api = { fetchImages };
-export default api;
+
+export default FetchImages;
